2df6af0b9b96f895db389965b8605d0f835b79d1
Bradley Beddoes <bradleybeddoes@gmail.com>
2014-12-18 16:19:38 +1000

    Fix rubocop errors in spec/dummy
    Improve implementations and correct errors always produced by
    `rails new`.

--- spec/dummy/app/controllers/api/api_controller.rb
+++ spec/dummy/app/controllers/api/api_controller.rb
@@ -25,6 +25,14 @@ module API
     protected
 
     def ensure_authenticated!
+      # Ensure API subject exists and is functioning
+      @subject = APISubject.find_by!(x509_cn: x509_cn)
+      fail(Unauthorized, 'Subject not functional') unless @subject.functioning?
+    rescue ActiveRecord::RecordNotFound
+      raise(Unauthorized, 'Subject invalid')
+    end
+
+    def x509_cn
       # Verified DN pushed by nginx following successful client SSL verification
       # nginx is always going to do a better job of terminating SSL then we can
       x509_dn = request.headers['HTTP_X509_DN'].try(:force_encoding, 'UTF-8')
@@ -32,19 +40,13 @@ module API
 
       x509_dn_parsed = OpenSSL::X509::Name.parse(x509_dn)
       x509_dn_hash = Hash[x509_dn_parsed.to_a
-                          .map{ |components| components[0..1] }]
-      x509_cn = x509_dn_hash['CN']
-      fail(Unauthorized, 'Subject CN invalid') unless x509_cn
+                          .map { |components| components[0..1] }]
 
-      # Ensure API subject exists and is functioning
-      @subject = APISubject.find_by!(x509_cn: x509_cn)
-      fail(Unauthorized, 'Subject not functional') unless @subject.functioning?
-    rescue OpenSSL::X509::NameError
-      fail(Unauthorized, 'Subject DN invalid')
-    rescue ActiveRecord::RecordNotFound
-      fail(Unauthorized, 'Subject invalid')
+      x509_dn_hash['CN'] || fail(Unauthorized, 'Subject CN invalid')
+
+      rescue OpenSSL::X509::NameError
+        raise(Unauthorized, 'Subject DN invalid')
     end
-
 
     def check_access!(action)
       fail(Forbidden) unless @subject.permits?(action)
