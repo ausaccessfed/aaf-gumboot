19f2a3a2b751a7e0fcac377bcc487543c93d1742
Bradley Beddoes <bradleybeddoes@gmail.com>
2014-12-19 09:38:49 +1000

    Wrap all shared examples in a context
    This ensures that any global subject set in the implementation
    spec does not adversely impact what our shared examples expect subject
    to be.

--- lib/gumboot/shared_examples/roles.rb
+++ lib/gumboot/shared_examples/roles.rb
@@ -1,12 +1,14 @@
 RSpec.shared_examples 'Roles' do
-  subject { build :role }
+  context 'AAF shared implementation' do
+    subject { build :role }
 
-  it { is_expected.to be_valid }
-  it { is_expected.to respond_to(:api_subjects) }
-  it { is_expected.to respond_to(:permissions) }
+    it { is_expected.to be_valid }
+    it { is_expected.to respond_to(:api_subjects) }
+    it { is_expected.to respond_to(:permissions) }
 
-  it 'is invalid without a name' do
-    subject.name = nil
-    expect(subject).not_to be_valid
+    it 'is invalid without a name' do
+      subject.name = nil
+      expect(subject).not_to be_valid
+    end
   end
 end
--- lib/gumboot/shared_examples/subjects.rb
+++ lib/gumboot/shared_examples/subjects.rb
@@ -1,27 +1,29 @@
 RSpec.shared_examples 'Subjects' do
-  subject { build :subject }
+  context 'AAF shared implementation' do
+    subject { build :subject }
 
-  it { is_expected.to be_valid }
-  it { is_expected.to be_an(Accession::Principal) }
-  it { is_expected.to respond_to(:roles) }
-  it { is_expected.to respond_to(:permissions) }
-  it { is_expected.to respond_to(:permits?) }
-  it { is_expected.to respond_to(:functioning?) }
+    it { is_expected.to be_valid }
+    it { is_expected.to be_an(Accession::Principal) }
+    it { is_expected.to respond_to(:roles) }
+    it { is_expected.to respond_to(:permissions) }
+    it { is_expected.to respond_to(:permits?) }
+    it { is_expected.to respond_to(:functioning?) }
 
-  it 'is invalid without a name' do
-    subject.name = nil
-    expect(subject).not_to be_valid
-  end
-  it 'is invalid without mail' do
-    subject.mail = nil
-    expect(subject).not_to be_valid
-  end
-  it 'is invalid without an enabled state' do
-    subject.enabled = nil
-    expect(subject).not_to be_valid
-  end
-  it 'is invalid without a complete state' do
-    subject.complete = nil
-    expect(subject).not_to be_valid
+    it 'is invalid without a name' do
+      subject.name = nil
+      expect(subject).not_to be_valid
+    end
+    it 'is invalid without mail' do
+      subject.mail = nil
+      expect(subject).not_to be_valid
+    end
+    it 'is invalid without an enabled state' do
+      subject.enabled = nil
+      expect(subject).not_to be_valid
+    end
+    it 'is invalid without a complete state' do
+      subject.complete = nil
+      expect(subject).not_to be_valid
+    end
   end
 end
