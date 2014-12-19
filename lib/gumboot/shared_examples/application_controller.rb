8f274d2d71f85c5201682a607ca9d1877cfa2148
Bradley Beddoes <bradleybeddoes@gmail.com>
2014-12-19 12:32:34 +1000

    ApplicationController implementation and specs

---
+++ lib/gumboot/shared_examples/application_controller.rb
@@ -0,0 +1,133 @@
+require 'gumboot/shared_examples/anonymous_controller'
+
+RSpec.shared_examples 'Application controller' do
+  context 'AAF shared implementation' do
+    include_examples 'Anon controller'
+
+    before do
+      @routes.draw do
+        get '/anonymous/an_action' => 'anonymous#an_action'
+        get '/anonymous/bad_action' => 'anonymous#bad_action'
+        get '/anonymous/public' => 'anonymous#public'
+      end
+    end
+
+    it { is_expected.to respond_to(:subject) }
+
+    context '#before_action' do
+      subject { response }
+
+      context 'No subject ID is set in session' do
+        before { get :an_action }
+
+        it { is_expected.to have_http_status(:redirect) }
+        it { is_expected.to redirect_to('/auth/login') }
+      end
+
+      context 'session has subject_id that does not represent a Subject' do
+        before do
+          session[:subject_id] = -1
+          get :an_action
+        end
+
+        it { is_expected.to have_http_status(:unauthorized) }
+        it { is_expected.to render_template('errors/unauthorized') }
+
+        it 'resets the session' do
+          expect(session[:subject_id]).to be_nil
+        end
+      end
+
+      context 'Subject that is not functioning' do
+        let(:current_subject) { create :subject, enabled: false }
+
+        before do
+          session[:subject_id] = current_subject.id
+          get :an_action
+        end
+
+        it { is_expected.to have_http_status(:unauthorized) }
+        it { is_expected.to render_template('errors/unauthorized') }
+
+        it 'resets the session' do
+          expect(session[:subject_id]).to be_nil
+        end
+      end
+    end
+
+    context '#after_action' do
+      before { session[:subject_id] = subject.id }
+
+      RSpec.shared_examples 'base state' do
+        it 'fails request to incorrectly implemented action' do
+          msg = 'No access control performed by AnonymousController#bad_action'
+          expect { get :bad_action }.to raise_error(msg)
+        end
+
+        it 'completes request to a public action' do
+          get :public
+          expect(response).to have_http_status(:ok)
+        end
+      end
+
+      context 'subject without permissions' do
+        subject(:subject) { create :subject }
+
+        include_examples 'base state'
+
+        it 'has no permissions' do
+          expect(subject.permissions).to eq([])
+        end
+
+        context 'the request does not complete' do
+          before { get :an_action }
+          it 'should respond with status code :forbidden (403)' do
+            expect(response).to have_http_status(:forbidden)
+          end
+          it 'renders forbidden template' do
+            expect(response).to render_template('errors/forbidden')
+          end
+        end
+      end
+
+      context 'subject with invalid permissions' do
+        subject(:subject) do
+          create :subject, :authorized, permission: 'invalid:permission'
+        end
+
+        include_examples 'base state'
+
+        it 'has an invalid permission' do
+          expect(subject.permissions).to eq(['invalid:permission'])
+        end
+
+        context 'the request does not complete' do
+          before { get :an_action }
+          it 'should respond with status code :forbidden (403)' do
+            expect(response).to have_http_status(:forbidden)
+          end
+          it 'renders forbidden template' do
+            expect(response).to render_template('errors/forbidden')
+          end
+        end
+      end
+
+      context 'subject with valid permission' do
+        subject(:subject) do
+          create :subject, :authorized, permission: 'required:permission'
+        end
+
+        include_examples 'base state'
+
+        it 'has a valid permission' do
+          expect(subject.permissions).to eq(['required:permission'])
+        end
+
+        it 'completes request after permissions checked' do
+          get :an_action
+          expect(response).to have_http_status(:ok)
+        end
+      end
+    end
+  end
+end
--- spec/dummy/app/controllers/application_controller.rb
+++ spec/dummy/app/controllers/application_controller.rb
@@ -1,5 +1,53 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
+  Forbidden = Class.new(StandardError)
+  private_constant :Forbidden
+  rescue_from Forbidden, with: :forbidden
+
+  Unauthorized = Class.new(StandardError)
+  private_constant :Unauthorized
+  rescue_from Unauthorized, with: :unauthorized
+
   protect_from_forgery with: :exception
+  before_action :ensure_authenticated
+  after_action :ensure_access_checked
+
+  attr_reader :subject
+
+  protected
+
+  def ensure_authenticated
+    if session[:subject_id]
+      @subject = Subject.find(session[:subject_id])
+      fail(Unauthorized, 'Subject not functional') unless @subject.functioning?
+    else
+      redirect_to('/auth/login')
+    end
+  rescue ActiveRecord::RecordNotFound
+    raise(Unauthorized, 'Subject invalid')
+  end
+
+  def ensure_access_checked
+    return if @access_checked
+
+    method = "#{self.class.name}##{params[:action]}"
+    fail("No access control performed by #{method}")
+  end
+
+  def check_access!(action)
+    fail(Forbidden) unless subject.permits?(action)
+    @access_checked = true
+  end
+
+  def public_action
+    @access_checked = true
+  end
+
+  def unauthorized
+    reset_session
+    render 'errors/unauthorized', status: :unauthorized
+  end
+
+  def forbidden
+    render 'errors/forbidden', status: :forbidden
+  end
 end
