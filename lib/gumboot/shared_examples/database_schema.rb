c6544c7ebf10859a12ddca6cc226a40bb2d08a36
Shaun Mangelsdorf <s.mangelsdorf@gmail.com>
2016-03-08 14:17:41 +1000

    Refactor failure_message to avoid branching

--- lib/gumboot/shared_examples/database_schema.rb
+++ lib/gumboot/shared_examples/database_schema.rb
@@ -1,16 +1,11 @@
 RSpec.shared_examples 'Database Schema' do
   context 'AAF shared implementation' do
-    RSpec::Matchers.define :have_collation do |expected|
+    RSpec::Matchers.define :have_collation do |expected, name|
       match { |actual| actual[:Collation] == expected }
 
       failure_message do |actual|
-        if actual[:Field]
-          format('expected field `%<Field>s` to use collation `%<expected>s`,' \
-                 ' but was `%<Collation>s`', actual.merge(expected: expected))
-        else
-          format('expected table `%<Name>s` to use collation `%<expected>s`, ' \
-                 'but was `%<Collation>s`', actual.merge(expected: expected))
-        end
+        "expected #{name} to use collation #{expected}, but was " \
+          "#{actual[:Collation]}"
       end
     end
 
@@ -30,12 +25,15 @@ RSpec.shared_examples 'Database Schema' do
 
     it 'has the correct collation' do
       query('SHOW TABLE STATUS').each do |table|
-        next if table[:Name] == 'schema_migrations'
-        expect(table).to have_collation('utf8_bin')
+        table_name = table[:Name]
+        next if table_name == 'schema_migrations'
+        expect(table).to have_collation('utf8_bin', "`#{table_name}`")
 
         query("SHOW FULL COLUMNS FROM #{table[:Name]}").each do |column|
           next unless column[:Collation]
-          expect(column).to have_collation('utf8_bin')
+          expect(column)
+            .to have_collation('utf8_bin',
+                               " `#{table_name}`.`#{column[:Field]}`")
         end
       end
     end
