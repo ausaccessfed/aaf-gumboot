046b6b3cf92f26663b312a7bc2378c6aa49ce2a5
Jake Schoermer <jake@secretservers.net>
2016-04-22 13:16:37 +1000

    Refactor foreign key specs to conform to README doc
    Refactored the foreign key specs so that they can be included with a
    simple one-liner in the client app. This brings the app back into
    conformity with the current documentation.

--- lib/gumboot/shared_examples/foreign_keys.rb
+++ lib/gumboot/shared_examples/foreign_keys.rb
@@ -2,7 +2,7 @@ def conn
   ActiveRecord::Base.connection
 end
 
-RSpec.shared_examples 'Foreign Keys' do |from_table, to_table, fk|
+RSpec.shared_examples 'fk' do |from_table, to_table, fk|
   context 'Permissions Foreign Keys', if: conn.supports_foreign_keys? do
     it 'should have a foreign key' do
       expect(table_has_fk(from_table, fk)).to be_truthy
@@ -23,4 +23,17 @@ RSpec.shared_examples 'Foreign Keys' do |from_table, to_table, fk|
       fk[:to_table] == to_table && fk.options[:column] == foreign_key
     end
   end
+end
+
+RSpec.shared_examples 'Foreign Keys' do
+  include_examples 'fk', 'permissions', 'roles', 'role_id'
+  include_examples(
+    'fk',
+    'api_subject_roles',
+    'api_subjects',
+    'api_subject_id'
+  )
+  include_examples 'fk', 'api_subject_roles', 'roles', 'role_id'
+  include_examples 'fk', 'subject_roles', 'subjects', 'subject_id'
+  include_examples 'fk', 'subject_roles', 'roles', 'role_id'
 end
--- spec/gumboot/foreign_keys_spec.rb
+++ spec/gumboot/foreign_keys_spec.rb
@@ -3,14 +3,5 @@ require 'spec_helper'
 require 'gumboot/shared_examples/foreign_keys'
 
 RSpec.describe 'Foreign Keys' do
-  include_examples 'Foreign Keys', 'permissions', 'roles', 'role_id'
-  include_examples(
-    'Foreign Keys',
-    'api_subject_roles',
-    'api_subjects',
-    'api_subject_id'
-  )
-  include_examples 'Foreign Keys', 'api_subject_roles', 'roles', 'role_id'
-  include_examples 'Foreign Keys', 'subject_roles', 'subjects', 'subject_id'
-  include_examples 'Foreign Keys', 'subject_roles', 'roles', 'role_id'
+  include_examples 'Foreign Keys'
 end
